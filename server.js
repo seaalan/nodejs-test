7462a6c490c629919bae996605588d0a76e36dc2
DESKTOP-1D2STS8\Alex <alex@erealmsoft.com>
2019-03-27 12:33:29 +0800

    add index.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 var http = require("http");
 
-function onRequest(request, response) {
-    console.info('Request received.');
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("Hello World");
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        console.info('Request received.');
+        response.writeHead(200,{"Content-Type":"text/plain"});
+        response.write("Hello World");
+        response.end();
+    }
+    http.createServer(onRequest).listen(8989);
+    console.info('Server has started.');
 }
-http.createServer(onRequest).listen(8989)
+
+exports.start = start;
